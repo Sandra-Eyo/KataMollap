71ef3e2d00651aac7d347fa5c4b501811ffa95ee
Sandra-Eyo <seyo.smr@gmail.com>
2024-03-03 18:37:11 +0100

    Añadidos métodos para tareas y progrmadorTareas

--- src/main/java/intercepting/administrador/ProgramadorTareas.java
+++ src/main/java/intercepting/administrador/ProgramadorTareas.java
@@ -1,4 +1,28 @@
 package intercepting.administrador;
 
+import intercepting.targets.Target;
+import intercepting.filtres.Filtre;
+
 public class ProgramadorTareas {
+
+    private Tareas tareas = null;
+
+    public ProgramadorTareas() {};
+
+    public ProgramadorTareas(Target target) {
+        this.tareas = new Tareas();
+        this.tareas.setTarget(target);
+    }
+
+    public Tareas getTareas() {
+        return this.tareas;
+    }
+
+    public void setTarea(Filtre filtre) {
+        this.tareas.anadirTarea(filtre);
+    }
+
+    public void ejecutarTareas(String resultado) {
+    }
+
 }
--- src/main/java/intercepting/administrador/Tareas.java
+++ src/main/java/intercepting/administrador/Tareas.java
@@ -1,4 +1,35 @@
 package intercepting.administrador;
 
+import intercepting.filtres.Filtre;
+import intercepting.targets.Target;
+import java.util.ArrayList;
+
+
 public class Tareas {
+
+    private Target target = null;
+    private ArrayList<Filtre> filtres = new ArrayList<>();
+
+    public Tareas() {};
+
+    ArrayList<Filtre> getTareas() {
+        return this.filtres;
+    }
+
+    Target getTarget() {
+        return this.target;
+    }
+
+    public  void anadirTarea(Filtre filtre) {
+        this.filtres.add(filtre);
+    }
+
+    public void ejecucion(String resultado) {
+        this.filtres.forEach(e -> e.ejecucion(resultado));
+    }
+
+    public void setTarget(Target target) {
+        this.target = target;
+    }
+
 }
